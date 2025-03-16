1b6280f66abe854fd70036c80ce099df2f32a4b5
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-03-16 10:19:48 -0400

    Release version 0.3.2
    Adding latest tag

--- docker.build.sh
+++ docker.build.sh
@@ -1,8 +1,12 @@
 img=yt2jf_playsync
-tag=0.3.1
+tag=0.3.2
+tag_latest=true
 full_img=$img:$tag
 code_verison=$(git rev-parse HEAD)
 echo "[core]
 docker_version=$tag
 code_version=$code_verison" > version.txt
 docker build -t $full_img .
+if [[ "$tag_latest" == "true" ]]; then
+  docker tag $img:$tag $img:latest
+fi
