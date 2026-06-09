8d27b1c399db1f6cbcbf2c3b3f1db39a76f7373a
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2026-06-09 16:10:56 -0400

    upd

--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
-if git diff --quiet && git diff --cached --quiet; then
-    : # No uncommitted changes, can proceed
-else
-    echo "You have uncommitted changes! Resolve and then try again"
-    exit 3
-fi
+#if git diff --quiet && git diff --cached --quiet; then
+#    : # No uncommitted changes, can proceed
+#else
+#    echo "You have uncommitted changes! Resolve and then try again"
+#    exit 3
+#fi
 all_versions=$(git ls-remote --tags --sort='v:refname' https://github.com/serg-bloim/yt2jf_playsync.git 'v*' | cut -d/ -f3-)
 latest_version=$(echo "$all_versions" | tail -n1)
 echo "Latest version is: ${latest_version#v}"
 echo
 
 read -p "New version: " user_version
-pattern='^[0-9]+\.[0-9]+\.[0-9]+$'
+pattern='^[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?$'
 if ! [[ $user_version =~ $pattern ]]; then
     echo "Invalid input, should be like '3.4.5'"
     exit 1
@@ -22,10 +22,16 @@ if git rev-parse "$tag_name" >/dev/null 2>&1; then
 fi
 
 echo "Version is good $user_version"
-
-sed "2s/.*/tag=$user_version/" docker.build.sh > docker.build.sh.tmp && mv docker.build.sh.tmp docker.build.sh
+replace_docker_build_tag(){
+  sed "2s/.*/tag=$1/" docker.build.sh > docker.build.sh.tmp && mv docker.build.sh.tmp docker.build.sh
+}
+replace_docker_build_tag $user_version
 git add docker.build.sh
 git commit -m "Release version $user_version"
 git tag "$tag_name"
+
+replace_docker_build_tag "SNAPSHOT"
+git add docker.build.sh
+git commit -m "Prepare for the next version"
 
 echo "Release is ready for a push"
